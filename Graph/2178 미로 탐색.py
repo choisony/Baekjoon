5efea879f2386dd6dd8ae53f253c9f48227ca5b2
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-10-02 20:58:41 +0900

    dfs로 풀었다가 bfs로 성공

--- Graph/2178 미로 탐색.py
+++ Graph/2178 미로 탐색.py
@@ -1,23 +1,29 @@
 import sys
-sys.setrecursionlimit(10**9)
+from collections import deque
 
 nx = [0,1,0,-1]
 ny = [1,0,-1,0]
 
-def dfs(x,y):
-    if x>=0 and x<n and y>=0 and y<m:
-        if d[x][y] == 1 :
-            for i in range(4):
-                if x+nx[i]>=0 and x+nx[i]<n and y+ny[i]>=0 and y+ny[i]<m and d[x+nx[i]][y+ny[i]]==1:
-                    if visited[x][y]+1 < visited[x+nx[i]][y+ny[i]]:
-                        visited[x+nx[i]][y+ny[i]] = visited[x][y]+1
-                        dfs(x+nx[i],y+ny[i])
+def bfs(a,b):
+    q = deque()
+    q.append((a,b))
+    while(q):
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            if (0<=nx<n) and (0<=ny<m):
+                if d[nx][ny] !=0 and visited[nx][ny]==99999:
+                    q.append((nx,ny))
+                    visited[nx][ny] = min(visited[nx][ny],visited[x][y]+1)
 
 
 
 
 n,m = map(int,sys.stdin.readline().split())
 d=[]
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
 for i in range(n):
     temp =[]
     line = input()
@@ -26,5 +32,5 @@ for i in range(n):
     d.append(temp)
 visited = [[99999]*m for i in range(n)]
 visited[0][0]= 1
-dfs(0,0)
+bfs(0,0)
 print(visited[n-1][m-1])
