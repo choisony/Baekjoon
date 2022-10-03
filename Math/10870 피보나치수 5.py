375be7d93ae432026efd0c0d5afbcab8ef285a0d
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-10-03 16:14:58 +0900

    O

---
+++ Math/10870 피보나치수 5.py
@@ -0,0 +1,15 @@
+n = int(input())
+d=[0]*(n+1)
+d[0]=0
+if n==0:
+    print(d[n])
+
+elif n==1:
+    d[1] = 1
+    print(d[n])
+else:
+    d[1] = 1
+    for i in range(2,n+1):
+        d[i] = d[i-1]+d[i-2]
+
+    print(d[n])
