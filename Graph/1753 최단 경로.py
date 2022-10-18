acad7096ec613ca076a830afec2d9af4f23bd2a6
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-10-19 01:56:46 +0900

    다익스트라는 Priority Queue보단 heapq를 사용하는 것이 Best같다

--- Graph/1753 최단 경로.py
+++ Graph/1753 최단 경로.py
@@ -36,3 +36,52 @@ for i in dist[1:]:
         print("INF")
     else:
         print(i)
+
+
+################# Priority Queue 시간초과 #################
+# from queue import PriorityQueue
+# import sys
+# n,e = map(int,sys.stdin.readline().split())
+# start = int(input())
+# graph2 = {} # 딕셔너리로 {1: [[1,1],[2,3]] } 이런식으로 가중치 있는거만 골라야함
+# for i in range(1,n+1):
+#     graph2[i] = {}
+# for _ in range(e):
+#     u,v,w = map(int,sys.stdin.readline().split())
+#
+#     if v in graph2[u]:
+#         graph2[u][v] = min(graph2[u][v],w)
+#     else:
+#         graph2[u][v] = w
+#
+# max_num = float('inf')
+# dist= [max_num]* (n+1)
+# visited = [False] * (n+1)
+# dist[start] = 0
+# q = PriorityQueue()
+# q.put([0, start])
+# def go(start):
+#     while not q.empty():
+#         weight, next= q.get()
+#         if dist[next] < weight:
+#             continue
+#         if visited[next] ==False:
+#             visited[next] = True
+#             if graph2[next]:
+#                 for next2,weight2 in graph2[next].items():
+#                     cost = dist[next]+weight2
+#                     if dist[next2] > cost:
+#                         dist[next2] = cost
+#                         if visited[next2]==False:
+#                             q.put([cost,next2])
+#
+# go(start)
+#
+# for i in dist[1:]:
+#     if i == max_num:
+#         print("INF")
+#     else:
+#         print(i)
+
+
+
