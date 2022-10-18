f4a6b6def8608f077c8b709c71e1a3c3b6173b88
choi sungmin <89989648+choisony@users.noreply.github.com>
2022-10-19 01:48:18 +0900

    O

--- Graph/1753 최단 경로.py
+++ Graph/1753 최단 경로.py
@@ -1,44 +1,38 @@
-from collections import deque
-
-n, e = map(int,input().split())
+import heapq
+import sys
+n, e = map(int,sys.stdin.readline().split())
 start = int(input())
-# graph=  [[0]* (n+1) for i in range(n+1)] # 이거로 풀면 메모리 초과
-graph2 = {} # 딕셔너리로 {1: [[1,1],[2,3]] } 이런식으로 가중치 있는거만 골라야함
+# graph=
+graph =  [[] for _ in range(n+1)]
+
 for _ in range(e):
-    u,v,w = map(int,input().split())
-    # graph[u][v] = w
-    if u in graph2:
-        graph2[u].append([v,w])
-    else:
-        graph2[u] = [[v,w]]
+    u,v,w = map(int,sys.stdin.readline().split())
+    graph[u].append((v,w))
 
+max_num = float('inf')
+dist= [max_num]* (n+1)
+visited = [False] * (n+1)
 
-visited= [99999999]* (n+1)
-
-visited[1] = 0
+dist[start] = 0
 def go(start):
-    q = deque()
-    q.append((start,0))
-    while(q):
-        next, weight= q.popleft()
-        if visited[next] < weight:
+    q = []
+    heapq.heappush(q,(0,start))
+    while q:
+        weight, next=  heapq.heappop(q)
+        if dist[next] < weight:
             continue
-        # for i in range(len(graph[next])):
-        #     if graph[next][i] !=0:
-        #         next2, weight2 = i,graph[next][i]
-        #         if visited[next2] > visited[next]+weight2:
-        #             visited[next2] = visited[next]+weight2
-        #             q.append(next2)
-        if next in graph2:
-            for i in graph2[next]:
-                next2, weight2 = i[0], i[1]
-                if visited[next2] > visited[next]+weight2:
-                    visited[next2] = visited[next]+weight2
-                    q.append((next2,weight2))
+        if visited[next] ==False:
+            visited[next] = True
+            for next2,weight2 in graph[next]:
+                cost = dist[next]+weight2
+                if dist[next2] > cost:
+                    dist[next2] = cost
+                    heapq.heappush(q,(cost,next2))
 
-go(1)
-for i in visited[1:]:
-    if i == 99999999:
+
+go(start)
+for i in dist[1:]:
+    if i == max_num:
         print("INF")
     else:
         print(i)
